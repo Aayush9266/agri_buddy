a18701f4f8bb476c0520e4bc999db8e5ac20522a
him <basvathimanshu@gmail.com>
2025-04-09 16:25:44 +0530

    Initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:agri_buddy/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Onboarding page loads correctly', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Example: Check for a title or button present in OnboardingPage
+    expect(
+        find.text('Welcome'), findsOneWidget); // change this based on your UI
   });
 }
